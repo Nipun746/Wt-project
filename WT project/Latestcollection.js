395cb2db3a4c2d028815efc4c63f5cbe06f2caed
Nipun746 <sachdevanipun28@gmail.com>
2024-10-19 12:23:59 +0530

    Add files via upload

---
+++ WT project/Latestcollection.js
@@ -0,0 +1,76 @@
+
+export const latestCollectionItems = [
+    {
+        img: 'https://i.pinimg.com/236x/51/d7/4b/51d74b3e093e4f8c4191a700579eb2cb.jpg',
+        name: 'Casual Shirts for Men|| Shirt for Men',
+        rating:{
+            star: '★★★★☆',
+            ratingNumber: 187
+        },
+        price: 419
+    },
+    {
+        img: 'https://m.media-amazon.com/images/I/71kE75JMKjL._AC_UL320_.jpg',
+        name: 'Casual Shirt for Men|| Shirt for Men',
+        rating:{
+            star: '★★★★☆',
+            ratingNumber: 187
+        },
+        price: 419
+    },
+    {
+        img: 'https://i.pinimg.com/564x/07/0d/25/070d25b8a007629132ffd5b8151cd60c.jpg',
+        name: 'Mens flare Pants',
+        rating:{
+            star: '★★★★☆',
+            ratingNumber: 195
+        },
+        price: 899
+    },
+    {
+        img: 'https://i.pinimg.com/564x/d6/bf/6b/d6bf6b9fed961ed7a291828808991cdd.jpg',
+        name: 'Full Size Tassel Wide Leg Pants',
+        rating:{
+            star: '★★★★☆',
+            ratingNumber: 222
+        },
+        price: 999
+    },
+    {
+        img: 'https://i.pinimg.com/564x/41/19/1d/41191d1be951ae85c4458425ebbfcc80.jpg ',
+        name: 'check designed pants',
+        rating:{
+            star: '★★★★☆',
+            ratingNumber: 159
+        },
+        price: 699
+    },
+    {
+        img: 'https://i.pinimg.com/564x/5d/06/f5/5d06f5b9fa2748a80ead6cfa1655971e.jpg',
+        name: 'Arlen® | Casual and Effortless general Top',
+        rating:{
+            star: '★★★★☆',
+            ratingNumber: 365
+        },
+        price: 799
+    },
+    {
+        img: 'https://i.pinimg.com/564x/46/e0/19/46e0198f13db37ddf580f126b90437d2.jpg',
+        name: 'Vintage Men Short Sleeve Shirt - Blanc et Noir / XL',
+        rating:{
+            star: '★★★★☆',
+            ratingNumber: 365
+        },
+        price: 699
+    },
+    {
+        img: 'https://i.pinimg.com/564x/fb/ff/79/fbff79d6d19f88968ad6771374cf62a6.jpg  ',
+        name: 'stylish blue top',
+        rating:{
+            star: '★★★★☆',
+            ratingNumber: 145
+        },
+        price: 499
+        
+    }
+]
---
+++ WT project/bestseller.js
@@ -0,0 +1,80 @@
+let bestSeller = [
+    {
+        img: 'https://m.media-amazon.com/images/I/51-pLhPHoBL.jpg',
+        name: 'Casual Shirts for Men|| Shirt for Men',
+        rating:{
+            star: '★★★★☆',
+            ratingNumber: 987
+        },
+        price: 599
+    },
+    {
+        img: 'https://m.media-amazon.com/images/I/61Q+oTHo0EL._SY741_.jpg',
+        name: 'Casual cargo || Bottom unisex',
+        rating:{
+            star: '★★★★☆',
+            ratingNumber: 785
+        },
+        price: 799
+    }
+    ,
+    {
+        img: 'https://i.pinimg.com/564x/a1/39/45/a13945f69b3fd94cc2b22c8e45364f30.jpg',
+        name: 'Polo shirt men',
+        rating:{
+            star: '★★★★☆',
+            ratingNumber: 785
+        },
+        price: 799
+    }
+    ,
+    {
+        img: 'https://i.pinimg.com/564x/5b/97/e0/5b97e013eca6e02225d55d100d79c84a.jpg',
+        name: 'T shirt women',
+        rating:{
+            star: '★★★★☆',
+            ratingNumber: 785
+        },
+        price: 799
+    }
+    ,
+    {
+        img: 'https://i.pinimg.com/enabled_lo/236x/69/2b/0a/692b0a2cda399231b0d5685e3615b497.jpg',
+        name: 'Children T shirt',
+        rating:{
+            star: '★★★★☆',
+            ratingNumber: 785
+        },
+        price: 799
+    }
+    ,
+    {
+        img: 'https://i.pinimg.com/236x/04/85/02/048502a1fe5da9bfa3f067f452cbab11.jpg',
+        name: 'Yellow bird design shirt children ',
+        rating:{
+            star: '★★★★☆',
+            ratingNumber: 785
+        },
+        price: 799
+    }
+    ,
+    {
+        img: 'https://i.pinimg.com/236x/96/72/71/967271987ba174bd47c4b3a4ca7c2827.jpg',
+        name: 'T- shirt for men',
+        rating:{
+            star: '★★★★☆',
+            ratingNumber: 785
+        },
+        price: 799
+    }
+    ,
+    {
+        img: 'https://i.pinimg.com/enabled_lo/564x/8c/58/6a/8c586a970b043e42e126b7d836f4d5d7.jpg',
+        name: 'waffle knit shirt for men ',
+        rating:{
+            star: '★★★★☆',
+            ratingNumber: 785
+        },
+        price: 799
+    }
+]
